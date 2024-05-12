684db22daf818612c7cabb64b8cfb0ccfbf4d16f
ANDyouNo <younofl312@gmail.com>
2024-05-12 13:20:14 +0300

    -

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "61c36c4b1d8a",
+    project: "javascript-react"
+  })],
+
+  base: '/iClone/',
+
+  build: {
+    sourcemap: true
+  }
+})
